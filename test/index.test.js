f8618354b01a350a37814f6b87ab6c9a76d9bb13
Rafael Lima <rvlima@daitangroup.com>
2019-02-28 10:27:00 -0300

    update deps version and add cors

--- test/index.test.js
+++ test/index.test.js
@@ -1,40 +1,44 @@
-'use strict'
+"use strict";
 
-const assert = require('assert')
-const graphql = require('graphql')
-const lambdaGraphql = require('../index')
+const assert = require("assert");
+const graphql = require("graphql");
+const lambdaGraphql = require("../index");
 
 const schema = graphql.buildSchema(`
   type Query {
-    hello(foo: String): String
+    hello: String
   }
-`)
+`);
 
-const root = { hello: (params) => 'Hello world! ' + (params.foo || '') }
+const root = {
+  hello: function() {
+    return "Hello world!";
+  },
+};
 
 const app = lambdaGraphql({
   schema: schema,
-  rootValue: root
-})
+  rootValue: root,
+});
 
-describe('handler', () => {
-  it('should handle get request',(done) => {
+describe("handler", () => {
+  it("should handle get request", done => {
     let event = {
-      "path": "/",
-      "httpMethod": "GET",
+      path: "/",
+      httpMethod: "GET",
       queryStringParameters: {
-        query: encodeURIComponent('query ($blah: String){hello(foo:$blah)}'),
-        variables: '{"blah":"test"}'
-      }
-    }
-    app.handler(event,{
-      succeed: (result) => {
-        assert.equal(result.statusCode,200)
-        let body = JSON.parse(result.body)
-        assert.equal('Hello world! test', body.data.hello)
-        done()
+        query: "{hello}",
       },
-      failed: done
-    })
-  })
-})
+    };
+    app.handler(event, {
+      succeed: result => {
+        assert.equal(result.statusCode, 200);
+        let body = JSON.parse(result.body);
+        assert.equal("Hello world!", body.data.hello);
+        done();
+        app.close();
+      },
+      failed: done,
+    });
+  });
+});
